19fe907b42ec515707cd735af88ead210ff1ee40
Oscar Santana <oscarsantana2014@gmail.com>
2021-09-13 16:13:17 -0500

    Entering Database Design

--- migration.sql
+++ migration.sql
@@ -20,3 +20,27 @@ CREATE TABLE ads (
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE mainCategory (
+                         id INT UNSIGNED AUTO_INCREMENT,
+                         category VARCHAR(100) NOT NULL,
+                         PRIMARY KEY(id),
+                         UNIQUE (category)
+);
+
+CREATE TABLE subCategory (
+                        id INT UNSIGNED AUTO_INCREMENT,
+                        main_id INT UNSIGNED NOT NULL,
+                        category VARCHAR(100) NOT NULL,
+                        PRIMARY KEY(id),
+                        FOREIGN KEY (main_id) REFERENCES mainCategory(id)
+);
+CREATE TABLE adCategories (
+                              ad_id INT UNSIGNED NOT NULL,
+                              mainCat_id INT UNSIGNED NOT NULL,
+                              subCat_id INT UNSIGNED NOT NULL,
+                              FOREIGN KEY (ad_id) REFERENCES ads(id),
+                              FOREIGN KEY (mainCat_id) REFERENCES mainCategory(id),
+                              FOREIGN KEY (subCat_id) REFERENCES subCategory(id)
+);
+
