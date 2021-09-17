c8d54b0cf05a18b84343f3b4e19edb1c15234ea5
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-09-17 12:06:30 -0500

    updated

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,10 +1,10 @@
 package com.codeup.adlister.dao;
 
 
-import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
 
 
+import com.codeup.adlister.util.Config;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
